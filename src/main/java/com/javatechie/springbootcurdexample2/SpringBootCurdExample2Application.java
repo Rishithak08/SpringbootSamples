783f5ef62ca319f69eeabe2c7deb69517db79a9e
Rishithak08 <rishithareddy5353@gmail.com>
2023-12-15 17:19:09 -0600

    my first commit

---
+++ src/main/java/com/javatechie/springbootcurdexample2/SpringBootCurdExample2Application.java
@@ -0,0 +1,13 @@
+package com.javatechie.springbootcurdexample2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCurdExample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCurdExample2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatechie/springbootcurdexample2/SpringBootCurdExample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.springbootcurdexample2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCurdExample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
